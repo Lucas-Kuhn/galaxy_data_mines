7822bd225c3c3f791100c282a1a2db31abeda95b
Lucas-Kuhn <84886844+Lucas-Kuhn@users.noreply.github.com>
2021-08-16 18:50:20 -0400

    Update user_agent.py

--- galaxy_data_mines/user_agent.py
+++ galaxy_data_mines/user_agent.py
@@ -58,9 +58,8 @@ def main(ctx, log, glossary,
     dir_exists = False
     if savelog or savetable or saveplot or savestats:
         currentDT = datetime.datetime.now()
-        filename = (currentDT.strftime("%Y-%m-%d|%Hhr-%Mm-%Ss")) + "-gdm"
+        filename = 'namegdm'
         ctx.obj['filename'] = filename
-        filename2 = 'objname' # added
 
         try:
             # Succeeds even if directory exists.
@@ -317,7 +316,7 @@ def common_option_handler(ctx, dc):
     if ctx.obj["showtable"]:
         dc.combined_table.show_in_browser(jsviewer=True)
     if ctx.obj["savetable"]:
-        dc.saveTable(fileName=filename2,
+        dc.saveTable(fileName=filename,
                      file_format=ctx.obj['savetable'])
 
     # If plot option(s) present.
